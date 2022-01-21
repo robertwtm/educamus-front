61fa975915e7d2279777c43d8b2d0a023f6a6388
GiovanaBorges <gigi12345as17@gmail.com>
2022-01-21 10:18:25 -0300

    @GiovanaBorges add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent } from "./inicio/inicio.component";
+import {CadastroComponent} from "./cadastro/cadastro.component";
+import { LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: "", redirectTo: "initial", pathMatch: "full"},
+  {path: "signUp", component: CadastroComponent},
+  {path: "signIn", component:  LoginComponent},
+  {path: "initial" , component : InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
