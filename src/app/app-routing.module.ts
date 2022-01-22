483c3308b1c582f6d68a82e69c6be9c52879d3ef
GiovanaBorges <gigi12345as17@gmail.com>
2022-01-21 22:49:57 -0300

    @GiovanaBorges changing route and add other

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {InicioComponent } from "./inicio/inicio.component";
 import {CadastroComponent} from "./cadastro/cadastro.component";
 import { LoginComponent} from "./login/login.component";
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   
   {path: "", redirectTo: "initial", pathMatch: "full"},
+  {path: "feed", component: FeedComponent},
   {path: "signUp", component: CadastroComponent},
   {path: "signIn", component:  LoginComponent},
   {path: "initial" , component : InicioComponent}
