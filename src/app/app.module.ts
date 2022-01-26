857e0548a153f412afb7b29573731e8e2a7e3832
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-01-26 18:28:10 -0300

    #12 - Tarefa 10 do PI - Segurança no Angular [@robertwtm]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { FeedComponent } from './feed/feed.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import { FeedComponent } from './feed/feed.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
