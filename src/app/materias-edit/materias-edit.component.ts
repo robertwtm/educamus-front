b755b59f5fa72621b4963cda74a57fb6ff76cc9b
GiovanaBorges <gigi12345as17@gmail.com>
2022-01-28 16:05:37 -0300

    @GiovanaBorges add editMateria

---
+++ src/app/materias-edit/materias-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-materias-edit',
+  templateUrl: './materias-edit.component.html',
+  styleUrls: ['./materias-edit.component.css']
+})
+export class MateriasEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
