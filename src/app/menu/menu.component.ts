aeee78e0edce21334adb5697b8a13222af4ebb64
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-01-31 16:57:09 -0300

    Crud Alune @robertwtm

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,10 +8,26 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+<<<<<<< Updated upstream
 
   id = environment.id
 
   constructor() {}
+=======
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  sair(){
+    this.router.navigate(['/initial'])
+    environment.token = ''
+    environment.nome = ''
+    environment.foto = ''
+    environment.id = 0
+  }
+>>>>>>> Stashed changes
+
 }
+
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -21,8 +22,19 @@ export class AuthService {
     return this.http.post<User>('https://app-educamus.herokuapp.com/usuarios/cadastrar', user)
   }
 
+<<<<<<< Updated upstream
   getByIdUser(id: number): Observable<User>{
     return this.http.get<User>(`https://app-educamus.herokuapp.com/usuarios/${id}`)
+=======
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+>>>>>>> Stashed changes
   }
 
 }
