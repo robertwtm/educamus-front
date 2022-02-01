d615f319d49d0ed1a50a224ef658b067a1a3442b
Augusto Cardoso Xavier <augustocx15@gmail.com>
2022-02-01 09:33:09 -0300

    modificação app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   { path: 'signUp', component: CadastroComponent },
   { path: 'signIn', component: LoginComponent },
   { path: 'initial', component: InicioComponent },
-  { path: 'perfilAluno', component: PerfilAlunoComponent },
+  { path: 'perfilAluno/:id', component: PerfilAlunoComponent },
   { path: 'perfilProf', component: PerfilProfComponent },
   { path: 'materiasEdit', component: MateriasEditComponent },
   { path: 'chatAluno', component: ChatAlunoComponent },
